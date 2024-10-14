df251d529df54ebcec1ad0a56f6d20d216ddb301
asutosh <asutoshbiswal658@gmail.com>
2024-10-14 20:19:07 +0530

    value injection through properties file

---
+++ IOCProj12-PropertiesFile-ValueInjection/src/main/java/com/nt/main/ValueAnnotationTest.java
@@ -0,0 +1,39 @@
+package com.nt.main;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.nt.config.AppConfig;
+import com.nt.sbeans.PersonInfo;
+
+public class ValueAnnotationTest {
+
+	public static void main(String[] args) {
+		//create IOC container
+		AnnotationConfigApplicationContext ctx=
+				new AnnotationConfigApplicationContext(AppConfig.class);
+		
+		
+		
+			//get Spring bean class object reference
+			PersonInfo info=ctx.getBean("pInfo",PersonInfo.class);
+			System.out.println(info);
+			
+		/*	// get Environment  obj from the IOC container
+			Environment  env=ctx.getEnvironment();
+			System.out.println(" per.id key value is ::"+env.getProperty("per.id"));
+			System.out.println(" os.name key value is ::"+env.getProperty("os.name"));
+			System.out.println(" env obj class name::"+env.getClass());
+			
+			//get  spring bean class obj ref
+			TestInfo  info1=ctx.getBean("tInfo",TestInfo.class);
+			info1.displayData();*/
+		
+		
+		
+		
+		//close the container
+		ctx.close();
+
+	}
+
+}
